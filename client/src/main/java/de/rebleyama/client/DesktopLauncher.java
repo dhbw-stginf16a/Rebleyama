7384581d149ef1ca7ed00d378a43af53ef60043f
Jan-Robin Aumann <jan-robin@aumann.org>
2018-04-04 15:52:55 +0200

    Adjusted import for project structure

--- client/src/main/java/de/rebleyama/client/DesktopLauncher.java
+++ client/src/main/java/de/rebleyama/client/DesktopLauncher.java
@@ -2,7 +2,7 @@ package de.rebleyama.client;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import de.rebleyama.RebleyamaClient;
+import de.rebleyama.client.RebleyamaClient;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
