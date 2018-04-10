46298449454eaa42038b0a1d0fb60825e4fa0e71
Jan-Robin Aumann <jan-robin@aumann.org>
2018-04-10 15:22:01 +0200

    removed raindrop game in favor of a pregenerated map which you can navigate by using the arrow keys

--- client/src/main/java/de/rebleyama/client/RebleyamaClient.java
+++ client/src/main/java/de/rebleyama/client/RebleyamaClient.java
@@ -4,135 +4,127 @@ import java.util.Iterator;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.MathUtils;
 import com.badlogic.gdx.math.Rectangle;
 import com.badlogic.gdx.math.Vector3;
-import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.TimeUtils;
+import com.badlogic.gdx.maps.*;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.assets.loaders.resolvers.LocalFileHandleResolver;
 
-public class RebleyamaClient extends ApplicationAdapter {
-	private Texture dropImage;
-	private Texture bucketImage;
-	private Sound dropSound;
-	private Music rainMusic;
+import java.util.logging.*;
+
+public class RebleyamaClient extends ApplicationAdapter implements InputProcessor{
+	
 	private SpriteBatch batch;
 	private OrthographicCamera camera;
-	private Rectangle bucket;
-	private Array<Rectangle> raindrops;
-	private long lastDropTime;
-	
+	private TiledMapRenderer tiledMapRenderer;
+	private TiledMap tiledMap;
+
 	@Override
 	public void create () {
-		// load the images for the droplet and the bucket, 64x64 pixels each
-		dropImage = new Texture(Gdx.files.local("assets/textures/droplet.png"));
-		bucketImage = new Texture(Gdx.files.local("assets/textures/bucket.png"));
 
-		// load the drop sound effect and the rain background "music"
-		dropSound = Gdx.audio.newSound(Gdx.files.local("assets/sounds/drop.wav"));
-		rainMusic = Gdx.audio.newMusic(Gdx.files.local("assets/sounds/rain.mp3"));
-		
-		// start the playback of the background music immediately
-		rainMusic.setLooping(true);
-		rainMusic.play();
-		
-		// create the camera and the SpriteBatch
-		camera = new OrthographicCamera();
-		camera.setToOrtho(false, 800, 480);
 		batch = new SpriteBatch();
-		
-		// create a Rectangle to logically represent the bucket
-		bucket = new Rectangle();
-		bucket.x = 800 / 2 - 64 / 2; // center the bucket horizontally
-		bucket.y = 20; // bottom left corner of the bucket is 20 pixels above the bottom screen edge
-		bucket.width = 64;
-		bucket.height = 64;
-		
-		// create the raindrops array and spawn the first raindrop
-		raindrops = new Array<Rectangle>();
-		spawnRaindrop();
-	}
 
-	private void spawnRaindrop() {
-		Rectangle raindrop = new Rectangle();
-		raindrop.x = MathUtils.random(0, 800-64);
-		raindrop.y = 480;
-		raindrop.width = 64;
-		raindrop.height = 64;
-		raindrops.add(raindrop);
-		lastDropTime = TimeUtils.nanoTime();
-	 }
+		//get the window size for the camera
+		float w = Gdx.graphics.getWidth();
+		float h = Gdx.graphics.getHeight();
 
-	@Override
-	 public void render() {
-		// clear the screen with a dark blue color. The
-		// arguments to glClearColor are the red, green
-		// blue and alpha component in the range [0,1]
-		// of the color to be used to clear the screen.
-		Gdx.gl.glClearColor(0, 0, 0.2f, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		// tell the camera to update its matrices.
+		//initiate the camera
+		camera = new OrthographicCamera();
+        camera.setToOrtho(false,w,h);
 		camera.update();
 		
-		// tell the SpriteBatch to render in the
-		// coordinate system specified by the camera.
+		//load the map
+		tiledMap = new TmxMapLoader().load("../client/assets/custommaps/orthogonal-outside.tmx");
+        tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
+		Gdx.input.setInputProcessor(this);
+	}
+
+	@Override
+	public void render() {
+		//clear background
+		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl20.glEnable(GL20.GL_BLEND);
+		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
 		batch.setProjectionMatrix(camera.combined);
+
+		//update the camera
+        camera.update();
 		
-		// begin a new batch and draw the bucket and
-		// all drops
-		batch.begin();
-		batch.draw(bucketImage, bucket.x, bucket.y);
-		for(Rectangle raindrop: raindrops) {
-		   batch.draw(dropImage, raindrop.x, raindrop.y);
-		}
-		batch.end();
-		
-		// process user input
-		if(Gdx.input.isTouched()) {
-		   Vector3 touchPos = new Vector3();
-		   touchPos.set(Gdx.input.getX(), Gdx.input.getY(), 0);
-		   camera.unproject(touchPos);
-		   bucket.x = touchPos.x - 64 / 2;
-		}
-		if(Gdx.input.isKeyPressed(Keys.LEFT)) bucket.x -= 200 * Gdx.graphics.getDeltaTime();
-		if(Gdx.input.isKeyPressed(Keys.RIGHT)) bucket.x += 200 * Gdx.graphics.getDeltaTime();
-		
-		// make sure the bucket stays within the screen bounds
-		if(bucket.x < 0) bucket.x = 0;
-		if(bucket.x > 800 - 64) bucket.x = 800 - 64;
-		
-		// check if we need to create a new raindrop
-		if(TimeUtils.nanoTime() - lastDropTime > 1000000000) spawnRaindrop();
-		
-		// move the raindrops, remove any that are beneath the bottom edge of
-		// the screen or that hit the bucket. In the later case we play back
-		// a sound effect as well.
-		Iterator<Rectangle> iter = raindrops.iterator();
-		while(iter.hasNext()) {
-		   Rectangle raindrop = iter.next();
-		   raindrop.y -= 200 * Gdx.graphics.getDeltaTime();
-		   if(raindrop.y + 64 < 0) iter.remove();
-		   if(raindrop.overlaps(bucket)) {
-			  dropSound.play();
-			  iter.remove();
-		   }
-		}
-	 }
-	 
-	 @Override
-	 public void dispose() {
+		//start up map renderer
+		tiledMapRenderer.setView(camera);
+        tiledMapRenderer.render();
+	}
+
+	@Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        if(keycode == Input.Keys.LEFT)
+            camera.translate(-32,0);
+        if(keycode == Input.Keys.RIGHT)
+            camera.translate(32,0);
+        if(keycode == Input.Keys.UP)
+            camera.translate(0,-32);
+        if(keycode == Input.Keys.DOWN)
+            camera.translate(0,32);
+        if(keycode == Input.Keys.NUM_1)
+            tiledMap.getLayers().get(0).setVisible(!tiledMap.getLayers().get(0).isVisible());
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+
+	@Override
+	public void dispose() {
 		// dispose of all the native resources
-		dropImage.dispose();
-		bucketImage.dispose();
-		dropSound.dispose();
-		rainMusic.dispose();
-		batch.dispose();
-	 }
+	}
 }
