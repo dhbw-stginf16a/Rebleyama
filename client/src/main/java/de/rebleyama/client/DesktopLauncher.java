825e3f1b1d93267e2b884971f09be29ab198b212
Jan-Robin Aumann <jan-robin@aumann.org>
2018-04-10 09:44:36 +0200

    implements simple game frame with a small sample game where you have to catch rain drops

--- client/src/main/java/de/rebleyama/client/DesktopLauncher.java
+++ client/src/main/java/de/rebleyama/client/DesktopLauncher.java
@@ -7,6 +7,9 @@ import de.rebleyama.client.RebleyamaClient;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "Rebleyama";
+		config.width = 800;
+		config.height = 480;
 		new LwjglApplication(new RebleyamaClient(), config);
 	}
 }
