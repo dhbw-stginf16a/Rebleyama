3ba43dbeccfb038b957334a7c7ac327262bc7cd1
Jan-Robin Aumann <jan-robin@aumann.org>
2018-04-11 12:37:29 +0200

    add back necessary imports

--- client/src/main/java/de/rebleyama/client/RebleyamaClient.java
+++ client/src/main/java/de/rebleyama/client/RebleyamaClient.java
@@ -7,6 +7,8 @@ import com.badlogic.gdx.InputProcessor;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
 import com.badlogic.gdx.maps.tiled.TmxMapLoader;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 import java.util.logging.*;
