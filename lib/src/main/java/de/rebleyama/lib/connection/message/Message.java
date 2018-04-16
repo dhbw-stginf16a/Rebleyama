8fd9137ac7a22de6dce60b4cc0f4cc663e18cdf3
Thore <thore@kruess.xyz>
2018-04-16 15:45:20 +0200

    Basic model deserialization

--- lib/src/main/java/de/rebleyama/lib/connection/message/Message.java
+++ lib/src/main/java/de/rebleyama/lib/connection/message/Message.java
@@ -9,4 +9,12 @@ public abstract class Message implements Serializable {
     private static final long serialVersionUID = -4605370810463332658L;
 	MessageType msgType;
     byte clientID;
+
+    /**
+     * Returns the client ID
+     * @return client ID
+     */
+    public byte getClientId() {
+        return clientID;
+    }
 }
