1dd3c5e18b2b9f7f04a9c4fb1bfc79ffeb168a1b
Jan-Robin Aumann <jan-robin@aumann.org>
2018-04-17 16:36:33 +0200

    Resolved merge conflicts arising from 0007 cleanup

--- client/src/main/java/de/rebleyama/client/RebleyamaClient.java
+++ client/src/main/java/de/rebleyama/client/RebleyamaClient.java
@@ -7,7 +7,6 @@ import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
 
 
-import javax.sound.midi.MidiDevice.Info;
 
 public class RebleyamaClient extends ApplicationAdapter implements InputProcessor {
 
