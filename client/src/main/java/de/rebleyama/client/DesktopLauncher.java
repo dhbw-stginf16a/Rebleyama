70ee12af341617f05c5c8aeded92ee2fe818a7fd
Oswaldbob <lennart.purucker@t-online.de>
2018-04-17 11:31:48 +0200

    Basics for UI + Minimap (#73)
    * Map Test, Client Camera position Test
    
    * Map Test, Smaller movements
    
    * Remove IML file
    
    * Try another Method
    
    * Added a pixmap Minimap
    
    * Added Documentation, changed position calculation based on actual window size
    
    * changed to global constant array, fixed x-y coordinate switch of minimap, added error check
    
    * added big map, m-key interaction and resize + variable window size
    
    * fixed minimap bug
    
    * implemented tile change function + example, other ui additions
    
    * Sonarlint adjustment + reformat
    
    * added comments + java doc for methods
    
    * windows now always spawn on top
    
    * refactor ui work to a new class
    
    * Removed todo list
    
    * Removed todo list

--- client/src/main/java/de/rebleyama/client/DesktopLauncher.java
+++ client/src/main/java/de/rebleyama/client/DesktopLauncher.java
@@ -2,19 +2,20 @@ package de.rebleyama.client;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import de.rebleyama.client.RebleyamaClient;
 
 public class DesktopLauncher {
 
-	private DesktopLauncher() {
-		
-	}
+    private DesktopLauncher() {
 
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		config.title = "Rebleyama";
-		config.width = 1280;
-		config.height = 720;
-		new LwjglApplication(new RebleyamaClient(), config);
-	}
+    }
+
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.title = "Rebleyama";
+        config.width = 1280;
+        config.height = 720;
+
+
+        new LwjglApplication(new RebleyamaClient(), config);
+    }
 }
--- client/src/main/java/de/rebleyama/client/RebleyamaClient.java
+++ client/src/main/java/de/rebleyama/client/RebleyamaClient.java
@@ -1,39 +1,34 @@
 package de.rebleyama.client;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
-import com.badlogic.gdx.InputProcessor;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.*;
+import com.badlogic.gdx.graphics.*;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.math.MathUtils;
-import com.badlogic.gdx.maps.tiled.TiledMap;
-import com.badlogic.gdx.maps.tiled.TiledMapRenderer;
-import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.*;
 import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
-import java.util.logging.*;
+
 
 public class RebleyamaClient extends ApplicationAdapter implements InputProcessor {
-	
-	private SpriteBatch batch;
-	private OrthographicCamera camera;
-	private TiledMapRenderer tiledMapRenderer;
-	private TiledMap tiledMap;
 
-	@Override
-	public void create () {
+    private SpriteBatch batch;
+    private OrthographicCamera camera;
+    private TiledMapRenderer tiledMapRenderer;
+    private TiledMap tiledMap;
 
-		batch = new SpriteBatch();
+    //Global Vars for UI
+    private ClientUI clientUI;
 
-		//get the window size for the camera
-		float w = Gdx.graphics.getWidth();
-		float h = Gdx.graphics.getHeight();
+    //start create method
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
 
-		//initiate the camera
-		camera = new OrthographicCamera();
-        camera.setToOrtho(false,w,h);
-		camera.update();
+        //get the window size for the camera
+        float w = Gdx.graphics.getWidth();
+        float h = Gdx.graphics.getHeight();
+        //initiate the camera
+        camera = new OrthographicCamera();
+        camera.setToOrtho(false, w, h);
+        camera.update();
         camera.position.x = 10240;
         camera.position.y = 10240;
 
@@ -42,117 +37,157 @@ public class RebleyamaClient extends ApplicationAdapter implements InputProcesso
         //also available: ../client/assets/custommaps/testMap.tmx
         tiledMap = new TmxMapLoader().load("../client/assets/custommaps/default.tmx");
         tiledMapRenderer = new OrthogonalTiledMapRenderer(tiledMap);
-		Gdx.input.setInputProcessor(this);
-	}
 
-	@Override
-	public void render() {
+        //create ui class
+        clientUI = new ClientUI(tiledMap);
+
+        //Creation of a Multiplexer which allows multi layer event handling (UI Layer and TiledMap Layer) (UI layer needs to be first ORDER IS IMPORTANT)
+        InputMultiplexer im = new InputMultiplexer(clientUI.getStage(), this);
+        Gdx.input.setInputProcessor(im);
+
+    }
+
+    //start render method
+    @Override
+    public void render() {
         //prevent camera from going out of bounds
         stayInBounds();
         //handle Input
-        handleMouseMovementInput();
         handleKeyMovementInput();
         handleKeyZoomInput();
-		//clear background
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl20.glEnable(GL20.GL_BLEND);
-		Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		
-		batch.setProjectionMatrix(camera.combined);
+        handleMouseMovementInput();
 
-		//update the camera
+        //clear background
+        Gdx.gl.glClearColor(1, 0, 0, 1);
+        Gdx.gl20.glEnable(GL20.GL_BLEND);
+        Gdx.gl.glBlendFunc(GL20.GL_SRC_ALPHA, GL20.GL_ONE_MINUS_SRC_ALPHA);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.setProjectionMatrix(camera.combined);
+
+        //update the camera
         camera.update();
 
-		//start up map renderer
+        //start up map renderer
         tiledMapRenderer.setView(camera);
         tiledMapRenderer.render();
+
+        // UI Render Part
+        clientUI.getStage().act(Gdx.graphics.getDeltaTime());
+        clientUI.getStage().draw();
     }
-    
-/**
- * Stub method for recognizing keypress
- * This triggers when any key is pressed down
- * @param keycode keycode of the key that was pressed
- */
-	@Override
+
+    //start event methods
+
+    /**
+     * Resize event which triggers on size change of client window
+     *
+     * @param width  Width after resize has happened
+     * @param height height after resize has happened
+     */
+    @Override
+    public void resize(int width, int height) {
+        clientUI.stageResize(width, height);
+    }
+
+    /**
+     * Stub method for recognizing keypress
+     * This triggers when any key is pressed down
+     *
+     * @param keycode keycode of the key that was pressed down
+     */
+    @Override
     public boolean keyDown(int keycode) {
+        //If ESC is pressed, show Menu
+        if (Gdx.input.isKeyPressed(Input.Keys.ESCAPE)) {
+            clientUI.uiKeypressed("escMenuWindow");
+            return true;
+        }
+        //If m is pressed, show Map
+        if (Gdx.input.isKeyPressed(Input.Keys.M)) {
+            clientUI.uiKeypressed("mapWindow");
+            return true;
+        }
         return false;
     }
 
-/**
- * Stub method for recognizing keypress
- * This triggers when any key is released
- * @param keycode keycode of the key that was released
- */
+    /**
+     * Stub method for recognizing keypress
+     * This triggers when any key is released
+     *
+     * @param keycode keycode of the key that was released
+     */
     @Override
     public boolean keyUp(int keycode) {
         return false;
     }
 
-/**
- * Stub method for recognizing keypress
- * This triggers when any key is typed (pressed and released)
- * @param character character that was typed
- */
+    /**
+     * Stub method for recognizing keypress
+     * This triggers when the mouse is moved
+     */
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    /**
+     * Stub method for recognizing keypress
+     * This triggers when any key is typed (pressed and released)
+     *
+     * @param character character that was typed
+     */
     @Override
     public boolean keyTyped(char character) {
         return false;
     }
 
-/**
- * Stub method for recognizing keypress
- * This triggers when screen is touched
- * 
- */
+    /**
+     * Stub method for recognizing keypress
+     * This triggers when screen is touched
+     */
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         return false;
     }
 
-/**
- * Stub method for recognizing keypress
- * This triggers when screen is released
- */
+    /**
+     * Stub method for recognizing keypress
+     * This triggers when screen is released
+     */
     @Override
     public boolean touchUp(int screenX, int screenY, int pointer, int button) {
         return false;
     }
 
-/**
- * Stub method for recognizing keypress
- * This triggers when the finger is dragged across the screen
- */
+    /**
+     * Stub method for recognizing keypress
+     * This triggers when the finger is dragged across the screen
+     */
     @Override
     public boolean touchDragged(int screenX, int screenY, int pointer) {
         return false;
     }
 
-/**
- * Stub method for recognizing keypress
- * This triggers when the mouse is moved
- */
-    @Override
-    public boolean mouseMoved(int screenX, int screenY) {
-        return false;
-    }
-   
-/**
- * Stub method for recognizing keypress
- * This triggers when the scrollwheel is scrolled
- * @param amount amount that the scrollwheel was moved
- */
+    /**
+     * Stub method for recognizing keypress
+     * This triggers when the scrollwheel is scrolled
+     *
+     * @param amount amount that the scrollwheel was moved
+     */
     @Override
     public boolean scrolled(int amount) {
         /*calculate the effective area of the map that is shown on screen (only calculating width
         because it will always be larger than height as soon as we enforce 16:9 aspect ration)*/
         float effectiveViewportWidth = camera.viewportWidth * camera.zoom;
-        
+
         /*check if the area shown on screen is still within the minimum (480 px -> 12 fields) and the
         maximum (20480 px -> 512 fields) bounds. If it is not, camera is not zoomed unless the zoom amount
-        is going to change the effective area shown away from the bound 
+        is going to change the effective area shown away from the bound
         */
         if (((effectiveViewportWidth < 20480) || amount < 0) && ((effectiveViewportWidth > 480) || amount > 0)) {
-            camera.zoom += (0.3*amount);
+            camera.zoom += (0.3 * amount);
+            return true;
         }
 
         return false;
@@ -161,49 +196,61 @@ public class RebleyamaClient extends ApplicationAdapter implements InputProcesso
     /**
      * dispose of all the native resources we allocated in create()
      */
-	@Override
-	public void dispose() {
-		tiledMap.dispose();
-		batch.dispose();
+    @Override
+    public void dispose() {
+        tiledMap.dispose();
+        batch.dispose();
+        clientUI.dispose();
     }
-    
+
+    //start own input handling methods
+
+    /**
+     * Handles Key Input for Movement
+     */
+    private void handleKeyMovementInput() {
+        //if key is pressed, move camera accordingly
+        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
+            camera.translate(-25, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
+            camera.translate(25, 0);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) {
+            camera.translate(0, -25);
+        }
+        if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) {
+            camera.translate(0, 25);
+        }
+    }
+
+    /**
+     * Handles Mouse Input for Movement
+     */
     private void handleMouseMovementInput() {
-        float mousePositionX = Gdx.input.getX(); 
+        float mousePositionX = Gdx.input.getX();
         float mousePositionY = Gdx.input.getY();
         float windowWidth = Gdx.graphics.getWidth();
         float windowHeight = Gdx.graphics.getHeight();
 
         //if mouse is in a 5% range of any of the edges of the window, move the camera accordingly
-        if (mousePositionY <= windowHeight/20) {
-            camera.translate(0,25*camera.zoom);
+        if (mousePositionY <= windowHeight / 20) {
+            camera.translate(0, 25 * camera.zoom);
         }
-        if (mousePositionY >= windowHeight - windowHeight/20) {
-            camera.translate(0,-25*camera.zoom);
+        if (mousePositionY >= windowHeight - windowHeight / 20) {
+            camera.translate(0, -25 * camera.zoom);
         }
-        if (mousePositionX >= windowWidth - windowWidth/20) {
-            camera.translate(25*camera.zoom,0);
+        if (mousePositionX >= windowWidth - windowWidth / 20) {
+            camera.translate(25 * camera.zoom, 0);
         }
-        if (mousePositionX <= windowWidth/20) {
-            camera.translate(-25*camera.zoom,0);
-        }
-    }
-
-    private void handleKeyMovementInput() {
-        //if key is pressed, move camera accordingly
-        if (Gdx.input.isKeyPressed(Input.Keys.LEFT) || Gdx.input.isKeyPressed(Input.Keys.A)) {
-            camera.translate(-25,0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.RIGHT) || Gdx.input.isKeyPressed(Input.Keys.D)) {
-            camera.translate(25,0);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.DOWN) || Gdx.input.isKeyPressed(Input.Keys.S)) {
-            camera.translate(0,-25);
-        }
-        if (Gdx.input.isKeyPressed(Input.Keys.UP) || Gdx.input.isKeyPressed(Input.Keys.W)) {
-            camera.translate(0,25);
+        if (mousePositionX <= windowWidth / 20) {
+            camera.translate(-25 * camera.zoom, 0);
         }
     }
 
+    /**
+     * Handles Key Input for Zoom
+     */
     private void handleKeyZoomInput() {
         /*calculate the effective area of the map that is shown on screen (only calculating width
         because it will always be larger than height as soon as we enforce 16:9 aspect ration)*/
@@ -211,7 +258,7 @@ public class RebleyamaClient extends ApplicationAdapter implements InputProcesso
 
         /*check if the area shown on screen is still within the minimum (480 px -> 12 fields) and the
         maximum (20480 px -> 512 fields) bounds. If it is not, camera is not zoomed unless the zoom amount
-        is going to change the effective area shown away from the bound 
+        is going to change the effective area shown away from the bound
         */
         if (Gdx.input.isKeyPressed(Input.Keys.EQUALS) && (effectiveViewportWidth > 480)) {
             camera.zoom -= 0.1;
@@ -221,26 +268,29 @@ public class RebleyamaClient extends ApplicationAdapter implements InputProcesso
         }
     }
 
+    /**
+     * Handles Camera to stay in bounds
+     */
     private void stayInBounds() {
         //calculate the effective area of the map that is shown on screen
         float effectiveViewportWidth = camera.viewportWidth * camera.zoom;
-		float effectiveViewportHeight = camera.viewportHeight * camera.zoom;
+        float effectiveViewportHeight = camera.viewportHeight * camera.zoom;
 
         /*Check if camera position is at any coordinate out of the boundaries of our 512x512 tile map.
         If it is, set it to the corresponding edge. Since this method is called in the render() method,
         this is checked for every frame.
         */
-        if (camera.position.x < 0 + effectiveViewportWidth/2) {
-            camera.position.x = 0 + effectiveViewportWidth/2;
+        if (camera.position.x < 0 + effectiveViewportWidth / 2) {
+            camera.position.x = 0 + effectiveViewportWidth / 2;
         }
-        if (camera.position.x > 20480 - effectiveViewportWidth/2) {
-            camera.position.x = 20480 - effectiveViewportWidth/2;
+        if (camera.position.x > 20480 - effectiveViewportWidth / 2) {
+            camera.position.x = 20480 - effectiveViewportWidth / 2;
         }
-        if (camera.position.y < 0 + effectiveViewportHeight/2) {
-            camera.position.y = 0 + effectiveViewportHeight/2;
+        if (camera.position.y < 0 + effectiveViewportHeight / 2) {
+            camera.position.y = 0 + effectiveViewportHeight / 2;
         }
-        if (camera.position.y > 20480 - effectiveViewportHeight/2) {
-            camera.position.y = 20480 - effectiveViewportHeight/2;
+        if (camera.position.y > 20480 - effectiveViewportHeight / 2) {
+            camera.position.y = 20480 - effectiveViewportHeight / 2;
         }
     }
 }
