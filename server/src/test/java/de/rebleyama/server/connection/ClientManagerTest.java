5bb9c980ec09cace69f8eb93cb4c300e44ccfd2d
Thore <thore@kruess.xyz>
2018-04-24 09:57:40 +0200

    Update tests accordingly

--- server/src/test/java/de/rebleyama/server/connection/ClientManagerTest.java
+++ server/src/test/java/de/rebleyama/server/connection/ClientManagerTest.java
@@ -2,6 +2,8 @@ package de.rebleyama.server.connection;
 
 import de.rebleyama.lib.Log;
 import de.rebleyama.lib.gamestate.GameStateUpdate;
+import de.rebleyama.lib.net.message.GamestateRequestMessage;
+import de.rebleyama.lib.net.message.Message;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -13,20 +15,21 @@ import static org.junit.Assert.assertTrue;
 public class ClientManagerTest {
     ClientManager clientManager;
     GameManager gameManager;
+    byte clientId;
 
     @Before
     public void startClientManager() {
         Log.setLogLevel(Level.FINEST);
-        byte clientId = (byte) 1;
+        this.clientId = (byte) 1;
         this.gameManager = gameManager;
         this.clientManager = new ClientManager(clientId, gameManager);
         this.clientManager.begin();
     }
 
     @Test
-    public void startAndStopClientManager() {
-        GameStateUpdate gameStateUpdate = new GameStateUpdate();
-        assertTrue("Client Manager queue accepts input", clientManager.addToQueue(gameStateUpdate));
+    public void addToCMQueue() {
+        Message message = new GamestateRequestMessage(this.clientId, "123456");
+        assertTrue("Client Manager queue accepts input", clientManager.addToQueue(message));
     }
 
     @After
