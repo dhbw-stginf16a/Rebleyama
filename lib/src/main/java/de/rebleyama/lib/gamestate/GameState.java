79a526521de077d1e2062dc88edb1e531d031fa6
Thore <thore@kruess.xyz>
2018-05-02 15:48:00 +0200

    Fill the game state with life

--- lib/src/main/java/de/rebleyama/lib/gamestate/GameState.java
+++ lib/src/main/java/de/rebleyama/lib/gamestate/GameState.java
@@ -1,15 +1,23 @@
 package de.rebleyama.lib.gamestate;
 
+import de.rebleyama.lib.game.Player;
+import de.rebleyama.lib.game.TileMap;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * This is the final class providing the game state object.
  */
 public class GameState {
-
+    private TileMap tileMap;
+    private Map<Byte, Player> players;
     /**
      * Creates a new GameState object
      */
     public GameState() {
-        // I have no idea yet.
+        this.tileMap = new TileMap(512, 512);
+        this.players = new HashMap<>();
     }
 
     /** Apply a received gameStateUpdate to the current gamestate
@@ -20,6 +28,48 @@ public class GameState {
     public boolean applyUpdate(GameStateUpdate gameStateUpdate) {
         // We will see
         return true;
+    }
+
+    /**
+     * Adds a new player to the game
+     * @param clientId the id of the connecting client, received by the handshake
+     * @param playerName the nickname of the player.
+     */
+    public void addPlayer(byte clientId, String playerName) {
+        this.players.put(clientId, new Player(playerName));
+    }
+
+    /**
+     * Removes a player from the gamestate
+     * @param clientId which disconnected
+     */
+    public void deletePlayer(byte clientId) {
+        this.players.remove(clientId);
+    }
+
+    /**
+     * Returns a single player object
+     * @param clientId id of the client to update
+     * @return the player object of the given id
+     */
+    public Player getPlayer(byte clientId) {
+        return this.players.get(clientId);
+    }
+
+    /**
+     * Returns the whole player map for easier batch processing
+     * @return Map of player data
+     */
+    public Map<Byte, Player> getPlayers() {
+        return this.players;
+    }
+
+    /**
+     * Get the tile map from the game state
+     * @return The tile map
+     */
+    public TileMap getTileMap() {
+        return tileMap;
     }
 
     /**
