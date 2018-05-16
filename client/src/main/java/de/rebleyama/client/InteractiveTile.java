d701ba3c9c8823ab7c11c012e6247a0366a7079b
Jan-Robin Aumann <jan-robin@aumann.org>
2018-05-16 13:39:14 +0200

    removed debug border grid

--- client/src/main/java/de/rebleyama/client/InteractiveTile.java
+++ client/src/main/java/de/rebleyama/client/InteractiveTile.java
@@ -1,19 +1,20 @@
 package de.rebleyama.client;
 
+import com.badlogic.gdx.scenes.scene2d.Actor;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
 import de.rebleyama.lib.game.Tile;
 
 import com.badlogic.gdx.graphics.Texture;
 
-public class InteractiveTile extends Image {
+public class InteractiveTile extends Actor {
 
     public Tile tile;
     public int x_coordinate;
     public int y_coordinate;
 
-    public InteractiveTile(Tile tile, Texture border, int x, int y) {
-        super(border);
+    public InteractiveTile(Tile tile, int x, int y) {
+        super();
         this.tile = tile;
         x_coordinate = x;
         y_coordinate = y;
